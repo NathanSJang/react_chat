afdbf1394c4590865417dedce64eabfbb2f02345
Nathan S Jang <nathan.s.jang@gmail.com>
2021-06-10 09:25:36 -0700

    before deployment-2

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = new ApolloServer({
 
 
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url, subscriptionsUrl }) => {
+server.listen().then(({ url, subscriptionsUrl }) => {
   console.log(`🚀 Server ready at ${url}`);
   console.log(`🚀 Subscription ready at ${subscriptionsUrl}`);
 
